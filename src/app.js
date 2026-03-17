d805727be8c742a3f5ff24e4feb5a5ceb0118c81
Anshraj11111 <162826050+Anshraj22@users.noreply.github.com>
2026-03-17 19:53:48 +0530

    fix: allow x-admin-secret header in CORS

--- src/app.js
+++ src/app.js
@@ -55,7 +55,7 @@ app.use(
     },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-admin-secret'],
     exposedHeaders: ['Content-Type', 'Authorization'],
   })
 );
